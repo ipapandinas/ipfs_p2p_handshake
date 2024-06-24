8a5d536504f3d2ba7c71dabe3652a2fc9c513ed0
Igor Papandinas <26460174+ipapandinas@users.noreply.github.com>
2024-06-24 15:56:46 +0200

    TCP connection stream setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpStream;
+
+const ADDRESS: &str = "127.0.0.1:4001";
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut _stream = TcpStream::connect(ADDRESS).await?;
+
+    Ok(())
 }
